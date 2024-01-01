ac127aadbb12db8c32a37c11954d0da52580710f
EmirhanBarlas <133690166+EmirhanBarlas@users.noreply.github.com>
2024-01-02 00:19:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class CalendarProgram
+{
+    static void Main()
+    {
+        DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+        Console.WriteLine("   Date                  Day");
+        Console.WriteLine("-------------------------------");
+        for (int i = 0; i < (int)startDate.DayOfWeek; i++)
+        {
+            Console.Write("   ");
+        }
+
+        while (startDate.Month == DateTime.Now.Month)
+        {
+            Console.Write($" {startDate.Day,2} ");
+            if (startDate.DayOfWeek == DayOfWeek.Saturday)
+                Console.WriteLine();
+            startDate = startDate.AddDays(1);
+        }
+        Console.ReadLine();
+    }
+}
